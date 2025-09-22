c34a583c843e2cc81f6d323e3391cf234b4f950b
shiva raju <shivaraju529@gmail.com>
2025-09-22 23:15:58 +0530

    3rd commit

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# no space before or equal
+PERSON1=siva
+PERSON2=Raju
+
+echo "$PERSON1= Hi $PERSON2 how are you ??"
+echo "$PERSON2= I am fine $PERSON1"
+echo "$PERSON1= what are you learing $PERSON2"
